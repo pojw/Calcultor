8c56e9f8bb15fff0240e3b1d7d023042e4b00163
pojw <jaylinhernandez123@icloud.com>
2023-01-19 21:41:05 -0500

    got all the buttons adding the correct letter and now all thats left is just the functions

--- java.js
+++ java.js
@@ -4,3 +4,18 @@ const display=document.querySelector("#display")
 
 display.textContent+=a
 }
+function deleteAll(){
+
+    const display=document.querySelector("#display")
+    
+    display.textContent=""
+    }
+
+function Delete_one(){
+    const display= document.querySelector("#display")
+    let word=display.textContent
+
+    let split_word=word.split("")
+    split_word.pop()
+   split_word= split_word.join("")
+    display.textContent=split_word}
