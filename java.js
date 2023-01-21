575bd022fa03e2b1f60cca58a02ca30126d9b755
pojw <jaylinhernandez123@icloud.com>
2023-01-21 00:51:38 -0500

    i have added the algebra behind it and made it look nicer, I still need to finish some small small bugs and making it even nicer

--- java.js
+++ java.js
@@ -3,12 +3,19 @@ function add(a){
 const display=document.querySelector("#display")
 
 display.textContent+=a
+if(one_opertion){
+    second_number=true
+}
+
 }
 function deleteAll(){
 
     const display=document.querySelector("#display")
     
     display.textContent=""
+    Period=false
+    one_opertion=false
+    second_number=false
     }
 
 function Delete_one(){
@@ -19,3 +26,58 @@ function Delete_one(){
     split_word.pop()
    split_word= split_word.join("")
     display.textContent=split_word}
+function period(a){
+    if(Period==false){
+        const display=document.querySelector("#display")
+
+        display.textContent+=a
+        Period=true 
+    }
+}
+
+function equal(){
+    const display = document.querySelector("#display")
+    let word = display.textContent;
+    let split=word.split(" ")
+    
+    if(second_number){
+    if(split[1]=="+"){
+        display.textContent=Number(split[0])+Number(split[2])
+ 
+    }
+    else if (split[1]=="*"){
+        display.textContent=Number(split[0])*Number(split[2])
+
+    }
+    else if (split[1]=="-"){
+        display.textContent=(Number(split[0])-Number(split[2])).toFixed(3)
+
+    } 
+    else if (split[1]=="/"){
+        display.textContent=(Number(split[0])/Number(split[2])).toFixed(3)
+
+    }
+}
+}
+let one_opertion=false
+let second_number=false
+let Period= false
+function operations(a){
+    Period=false
+    if(one_opertion==false){
+    const display=document.querySelector("#display")
+
+    display.textContent+=a
+     }
+    
+    if(one_opertion==true){
+        equal()
+        const display=document.querySelector("#display")
+
+        display.textContent+=a
+    }
+    one_opertion=true
+
+
+}
+
